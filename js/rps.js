cd7af04b4166d95ca5536086ac6917290bdafd94
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-14 14:53:35 -0500

    added computer choice

--- js/rps.js
+++ js/rps.js
@@ -1,5 +1,11 @@
+const choices = ["Rock", "Paper", "Scissors"]
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+function getComputerChoice() {
+ let a = (Math.random()*3)+1;
+ return choices[a];
+}
 
+getComputerChoice();
