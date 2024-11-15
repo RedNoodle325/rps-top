572c87bf289f2bb0cbbefc3780db63e758b391b3
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-14 22:53:20 -0500

    completed game

--- js/rps.js
+++ js/rps.js
@@ -1,56 +1,113 @@
-const choices = ["Rock", "Paper", "Scissors"]
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+// const choices = ["Rock", "Paper", "Scissors"]
+// const rock = document.getElementById("rock");
+// const paper = document.getElementById("paper");
+// const scissors = document.getElementById("scissors");
+// let playerScore = 0;
+// let cpuScore = 0;
+// let tieScore = 0;
+// let playerChoice = "Awaiting Choice";
+// let cpuChoice = "Awaiting Choice";
+
+// document.getElementById('score-msg').innerHTML = "Good Luck!"
+// document.getElementById('rock').addEventListener('click', function() {
+//   playGame("Rock");
+// });
+
+// document.getElementById("paper").addEventListener('click', function() {
+//   playGame("Paper");
+// });
+
+// document.getElementById('scissors').addEventListener('click', function() {
+//   playGame("Scissors");
+// });
+
+// document.getElementById("scoreboard").innerHTML = playerScore + "-" + cpuScore + "-" + tieScore;
+
+// function updateScoreboard(){
+//   document.getElementById("scoreboard").innerHTML = playerScore + "-" + cpuScore + "-" + tieScore;
+//   document.getElementById('p-choice').innerHTML = playerChoice;
+//   document.getElementById('c-choice').innerHTML = cpuChoice;
+// }
+// function getComputerChoice() {
+//  let a = Math.floor((Math.random()*3));
+//  return choices[a];
+// }
+
+// function resetGame(){
+//   playerChoice="";
+//   cpuChoice="";
+// }
+
+// function playGame(choice) {
+//   cpuChoice = getComputerChoice();
+//   playerChoice = choice;
+
+//   if (cpuChoice === playerChoice) {
+//     tieScore++;
+//     document.getElementById("score-msg").innerHTML = "Tie...but you're really a loser.";
+//   }
+//   else if (cpuChoice === "Rock" && playerChoice === "Scissors" || cpuChoice ==="Scissors" && playerChoice === "Paper" || cpuChoice === "Paper" && playerChoice === "Rock") {
+//     cpuScore++;
+//     document.getElementById("score-msg").innerHTML = "Fuckin Loser!";
+//   }
+//   else {
+//     playerScore++;
+//     document.getElementById("score-msg").innerHTML = "Great job I guess...";
+//   }
+//   updateScoreboard();
+// }
+
+const rps = ["Rock", "Paper", "Scissors"];
+let cpuChoice, playerChoice = "None";
+let gameCount = 0;
 let playerScore = 0;
-let cpuScore = 0;
-let tieScore = 0;
-let playerChoice = "";
-let cpuChoice = "";
+let cpuScore = 0
 
-document.getElementById('')
-document.getElementById('rock').addEventListener('click', function() {
-  playGame("Rock");
-});
-
-document.getElementById("paper").addEventListener('click', function() {
-  playGame("Paper");
-});
-
-document.getElementById('scissors').addEventListener('click', function() {
-  playGame("Scissors");
-});
-
-document.getElementById("scoreboard").innerHTML = playerScore + "-" + cpuScore + "-" + tieScore;
-function updateScoreboard(){
-  document.getElementById("scoreboard").innerHTML = playerScore + "-" + cpuScore + "-" + tieScore;
-}
-function getComputerChoice() {
- let a = Math.floor((Math.random()*3)+1);
- return choices[a];
+function generateCPU(){
+  a = Math.floor(Math.random()*3);
+  return rps[a];
 }
 
-function resetGame(){
-  playerChoice="";
-  cpuChoice="";
+function isValid(choice) {
+  if (choice === 1 || choice === 2 || choice === 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function playGame(choice) {
-  cpuChoice = getComputerChoice();
-  playerChoice = choice;
+  function playRound(user, cpu){
+    if (user === cpu) {
+      console.log('Tie!\n\nScore: ' + playerScore + ' - ' + cpuScore);
+    } 
+    else if (user === "Rock" && cpu === "Paper" ||user === "Paper" && cpu === "Scissors" ||user === "Scissors" && cpu === "Rock") {
+      cpuScore++;
+      console.log('Loser!\n\nScore: ' + playerScore + ' - ' + cpuScore);
+    } else {
+      playerScore++;
+      console.log('YOU WIN MUHFUCKA!\n\nScore: ' + playerScore + ' - ' + cpuScore);
+    }
+    gameCount++;
+  }
 
-  if (cpuChoice === playerChoice) {
-    tieScore++;
-    document.getElementById("score-msg").innerHTML = "Tie...but you're really a loser.";
+  function playGame(){
+    cpuScore, playerScore, gamecount = 0;
+    while (gameCount < 5){
+      cpuChoice = generateCPU();
+      let playerPrompt = parseInt(prompt("Choose your fighter!\n 1. Rock \n2. Paper \n3. Scissors"));
     
+      if(isValid){
+        playerChoice = rps[playerPrompt-1];
+        playRound(playerChoice, cpuChoice);
+      } else {
+        alert('Invalid entry, try again.');
+      }
+    }
+    if (playerScore > cpuScore) {
+      console.log('YOU WIN!');
+    } else if (playerScore === cpuScore) {
+      console.log('Tie');
+    } else {
+      console.log("You lose!");
+    }
   }
-  else if (cpuChoice === "Rock" && playerChoice === "Scissors" || cpuChoice ==="Scissors" && playerChoice === "Paper" || cpuChoice === "Paper" && playerChoice === "Rock") {
-    cpuScore++;
-    document.getElementById("score-msg").innerHTML = "Fuckin Loser!";
-  }
-  else {
-    playerScore++;
-    document.getElementById("score-msg").innerHTML = "CPU CHOICE: " + cpuChoice + " PLAYER CHOICE: " + playerChoice +"Great job I guess...";
-  }
-  updateScoreboard();
-}
